82deee62760b58a589dc43295f8640e750b091e0
Xavier M <golf4Xavier@gmail.com>
2022-07-11 15:23:56 -0400

    adding html and editing js script

--- docs/DonutMaker.js
+++ docs/DonutMaker.js
@@ -1,4 +1,27 @@
-for(let d = 0; d < 100; d++)
-{
-    console.count();
+document.getElementById("Bake");
+document.getElementById("Multiply");
+document.getElementById("AutoClick");
+document.getElementById("Reset");
+
+
+function Bake(){
+    for (let i = 0; i < 99; i++) {
+        console.count();
+        if(i = 100)
+        {
+            AutoClick();
+        }
+      }
+}
+
+function Multiply(){
+    Math.pow(1.2, DonutMultiplierCount);
+}
+
+function AutoClick(){
+    
+}
+
+function Reset(){
+
 }
